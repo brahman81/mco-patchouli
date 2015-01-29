c043b2c1e4037ce79d7f3bafbfb9c20e524f9bf6
Tom Llewellyn-Smith <tom@onixconsulting.co.uk>
2015-01-29 13:43:50 +0000

    improve update parsing

--- util/patchouli.rb
+++ util/patchouli.rb
@@ -46,15 +46,46 @@ module MCollective
                 end
             end
 
-            def self.apt_parse_updates
+            def self.get_updates()
                 require 'open3'
-                output = "\n" # prefix with a newline
+                output = {:packages => []}
                 Open3.popen3('apt-get update && apt-get upgrade --just-print') do |stdin, stdout, stderr, wait_thr|
                     stdout.each do |line|
                         if (match = line.match(/Inst\s+(\w+)\s+\[(?:\d+:)?([\w,\.,-]+)\]\s+\(((?:\d+:)?([\w,\.,-]+))/)) then
-                            output << "#{match[1]} - installed: #{match[2]} available: #{match[3]}\n"
+                            output[:packages].push("#{match[1]} - installed: #{match[2]} available: #{match[3]}")
+                        elsif (match = line.match(/(\d+)\s+upgraded,\s+(\d+)\s+newly\s+installed,\s+(\d+)\s+to\s+remove\s+and\s+(\d+)\s+not\s+upgraded\./)) then
+                            if match.length() > 4 then
+                            output[:upgradable]     = match[1]
+                            output[:new_installs]   = match[2]
+                            output[:removed]        = match[3]
+                            output[:held_back]      = match[4]
+                            else
+                                return false
+                            end
                         end
                     end
+                end
+                return output
+            end
+
+            def self.updates_available?(type = :upgradable)
+                updates = self.get_updates()
+                if updates[type.to_sym] then
+                    return true
+                else
+                    return false
+                end
+            end
+
+            def self.apt_parse_updates(verbose = false)
+                require 'open3'
+                output = "\n" # prefix with a newline
+                if updates = self.get_updates() then
+                    if verbose then
+                        output << updates[:packages].join("\n")
+                    end
+                    output << "#{updates[:upgradable]} packages available\n"
+                    output << "#{updates[:held_back]} packages held back\n"
                 end
                 return output
             end
@@ -64,8 +95,8 @@ module MCollective
                 output = "\n" # prefix with a newline
                 # assume yes, upgrade config only if it has not been modified locally.
                 Open3.popen3('export DEBIAN_FRONTEND=noninteractive; apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"') do |stdin, stdout, stderr, wait_thr|
-                    if stderr.to_s.empty? then
-                        output << "packages updated successfully"
+                    unless self.updates_available? then
+                        output << "upgrade successful"
                     else
                         output << "some errors were encountered please perform a manual check on this node"
                     end
@@ -78,8 +109,8 @@ module MCollective
                 output = "\n" # prefix with a newline
                 # assume yes, upgrade config only if it has not been modified locally.
                 Open3.popen3('export DEBIAN_FRONTEND=noninteractive; apt-get dist-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"') do |stdin, stdout, stderr, wait_thr|
-                    if stderr.to_s.empty? then
-                        output << "packages updated successfully"
+                    unless self.updates_available?('held_back') then
+                        output << "dist-upgrade successful"
                     else
                         output << "some errors were encountered please perform a manual check on this node"
                     end
